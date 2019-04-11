8cf4ba5b47b2fd8e5a7ff1c7fa9ec003501638a9
Dustin Slane <info@dustinslane.nl>
2019-04-11 16:35:10 +0200

    Added test

---
+++ HttpHandlerNativeTest/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Debug = CitizenFX.Core.Debug;
+
+namespace HttpHandlerNativeTest
+{
+    /// <summary>
+    /// Test case to make FXServer crash
+    ///
+    /// Using Mono 4.5.2
+    /// Using CFXServer 1164
+    /// </summary>
+    public class Test : BaseScript
+    {
+        private static HttpListener HttpListener;
+        public Test()
+        {
+            Debug.WriteLine("Test Loaded");
+            API.SetHttpHandler(new Action<dynamic, dynamic>(HandleHttpRequest));
+
+        }
+
+        private async void HandleHttpRequest(dynamic request, dynamic response)
+        {
+            Debug.WriteLine("Ha");
+        }
+    }
+}
